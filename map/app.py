06c7e0672e674d05d56d5e478b742bb862e28300
mismas125-9818 <mumintrol17812@gmail.com>
2025-11-14 23:48:33 +0100

    Oprava zabezpečenia

--- map/app.py
+++ map/app.py
@@ -93,4 +93,4 @@ def admin():
     return render_template('admin.html', helpers=helpers)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False)
